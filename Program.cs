d805253596938e6e3c2664434e9c243354f9dfc6
Siketyan <siketyan@sikeserver.com>
2017-01-23 17:39:27 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
