db166590d326f2184df88db0e25e0fb94aa334e4
Siketyan <siketyan@sikeserver.com>
2017-01-23 17:50:01 +0900

    アセンブリ情報を追加

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimpleTrim")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Siketyan.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
